cdf2bc4e26046555b903c9f9d4363beefcb397fe
TattariMounika <mounikatattari10@gmail.com>
2026-07-01 23:45:07 +0530

    Add files via upload

---
+++ python fundamentals day2.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#variables
+
+a=3
+b=5
+
+print(a+b)
+
+
+# In[ ]:
+
+
+#rules for declaring the variables:
+1: no namespace for declaring the variables
+2: a variable name not start with a number
+3:a variable should not have special character @ # $ &
+
+
+# In[5]:
+
+
+first name ='jyothi'      #rule 1
+print(first name)
+
+
+# In[6]:
+
+
+firstname ='jyothi'
+print(firstname)
+
+
+# In[7]:
+
+
+1name ='jyothi'     #rule 2
+print(1name)
+
+
+# In[8]:
+
+
+name1 ='jyothi'
+print(name1)
+
+
+# In[9]:
+
+
+$name ='jyothi'    #rule 3
+print($name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#introduction to datatypes in python
+string--------------------str
+numbers----------------------integers(int) and float (decimals---0.1,0.2)
+list-------------------list
+tuples--------------------tuple
+dictionaries---------------dict
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+classification of datatypes
+datatypes classified into 2 types
+1:mutable data types------------------ which can able to edit/alter-------flexible in nature
+2:immutable datatypes------------------which cannot do edit /alter---------fixed in nature
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to string datatypes :
+
+    defination:a string is nothing but a series of characters declared in quotes
+
+classification :it is a immutable datatype
+
+how do you define the string---------------?
+1:single quotes
+2:double quotes
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+name = 'jyothi'   #single quote
+print(name)
+
+
+# In[11]:
+
+
+name ="mounika"    #double quote
+print(name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to string methods
+
+
+# In[12]:
+
+
+type(name)
+
+
+# In[13]:
+
+
+type(name1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+fullname ="tattari mounika"
+print(fullname)
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+print(fullname.title())
+
+
+# In[16]:
+
+
+print(fullname.upper())     #uppercase
+
+
+# In[17]:
+
+
+print(fullname.lower())      #lowercase
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
