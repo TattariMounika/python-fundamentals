a44091a1b497a7b2dd231f0a5b3dcfc8ae584786
TattariMounika <mounikatattari10@gmail.com>
2026-07-02 21:11:55 +0530

    Add files via upload

---
+++ python fundamentals3.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#introduction to f strings
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+firstname ='tattari'
+lastname ='mounika'
+
+
+# In[ ]:
+
+
+#required to get the full name....
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#general syntax
+
+
+# In[ ]:
+
+
+message =f"you can give a custom message,{place_holder_1}........{place_holder_n}"
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+fullname =f"{firstname} {lastname}"
+
+print(fullname)
+
+
+# In[13]:
+
+
+message =f"keep it up {fullname.title()}"
+
+
+# In[14]:
+
+
+print(message)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+adding whitespaces to strings:
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+print("favourite_programming_language:pythonjavacjavascriptc++")
+
+
+# In[17]:
+
+
+print("favourite_programming_language:\npython\njava\nc\njavascript\nc++")
+
+
+# In[ ]:
+
+
+# \n is called a new line delimiter
+
+
+# In[18]:
+
+
+print("favourite_programming_language:\n\tpython\n\tjava\n\tcjavascript\n\tc++")
+
+
+# In[ ]:
+
+
+# \t is called as a tab delimiter
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+removing whitespaces from the strings:
+
+
+# In[ ]:
+
+
+
+
+
+# In[24]:
+
+
+name = "   java"
+
+print(name)
+
+
+# In[21]:
+
+
+name1 ="java      "
+print(name1)
+
+
+# In[25]:
+
+
+name.lstrip()
+
+
+# In[23]:
+
+
+name1.rstrip()
+
+
+# In[26]:
+
+
+name2 = "       java       "
+print(name2)
+
+
+# In[28]:
+
+
+name2.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#introduction to numbers
+
+
+# In[ ]:
+
+
+integers:
+
+
+# In[29]:
+
+
+num =123
+print(num)
+
+
+# In[30]:
+
+
+type(num)
+
+
+# 
+
+# In[32]:
+
+
+num1=0.1
+print(num1)
+
+
+# In[33]:
+
+
+type(num1)
+
+
+# In[ ]:
+
+
+#note 1 : python is the case sensitive language
+
+
+# In[ ]:
+
+
+#### is used for commenting code
+
